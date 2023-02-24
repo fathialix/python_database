a47bbe1f3b0665c68c2fc91886fcdecd5e0ff8b6
eMobilis <fathiabdikadir09@gmail.com>
2023-02-24 10:07:31 +0300

    displaystudent

--- display_users.py
+++ display_users.py
@@ -1,5 +1,10 @@
 from main import User
+from main import Student
 
 myusers = User.select()
 for user in myusers:
     print(user.name, user.email, user.password)
+
+mystudents = Student.select()
+for student in mystudents:
+    print(student.student_name, student.student_id, student.student_class)
